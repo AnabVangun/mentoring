ba30ee13d4e8bea95cf375fd11681c67a4e4e84a
AnabVangun <thibaut.lajoiemazenc@gmail.com>
2023-08-07 15:34:41 +0200

    fix concurrency-management issue in SimpleObservableViewModel

--- src/main/java/mentoring/viewmodel/base/SimpleObservableViewModel.java
+++ src/main/java/mentoring/viewmodel/base/SimpleObservableViewModel.java
@@ -1,8 +1,8 @@
 package mentoring.viewmodel.base;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CopyOnWriteArrayList;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 
@@ -11,7 +11,7 @@ import javafx.beans.Observable;
  */
 public abstract class SimpleObservableViewModel implements Observable{
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
-    private final List<InvalidationListener> listeners = new ArrayList<>();
+    private final List<InvalidationListener> listeners = new CopyOnWriteArrayList<>();
     
     @Override
     public final void addListener(InvalidationListener il) {
@@ -26,7 +26,6 @@ public abstract class SimpleObservableViewModel implements Observable{
     }
     
     protected final void notifyListeners(){
-        //FIXME: possible concurrency exception if addListener or removeListener is called during notifyListeners
         for(InvalidationListener listener : listeners){
             listener.invalidated(this);
         }
